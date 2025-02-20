bdf2a9875e2905dbd2db8be2882aeb1be74f65ab
itsDav804 <davidkumala84@gmail.com>
2025-02-20 16:08:07 +0700

    test pengurangan

--- main.py
+++ main.py
@@ -6,8 +6,13 @@ def penambahan(a,b):
 	total = a+b
 	return(total)
 
+def pengurangan(a,b): #test pengurangan
+	total = a-b
+	return(total)
+
 def main():
 	
 	print(penambahan(10,5))
+	print(pengurangan(9.3)) #test case
 
 main()
